16e0233b6c4d167a5212a817f214b26614e883da
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-11 00:40:20 +0200

    fin slider image main

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 var slide = new Array(
-    
-    "https://picsum.photos/600/400?random=2", 
-"https://picsum.photos/600/400?random=3",
- "https://picsum.photos/600/400?random=4", 
- "https://picsum.photos/600/400?random=5");
+
+    "https://picsum.photos/600/400?random=2",
+    "https://picsum.photos/600/400?random=3",
+    "https://picsum.photos/600/400?random=4",
+    "https://picsum.photos/600/400?random=5");
 var numero = 0;
 
 function ChangeSlide(sens) {
+    //boucler les  images qui se trouvent dans la data d'images voir scripts array 
+
     numero = numero + sens;
     if (numero < 0)
         numero = slide.length - 1;
     if (numero > slide.length - 1)
         numero = 0;
     document.getElementById("slide").src = slide[numero];
+    //ici en bas on peut slider par un timer 
+    //setInterval("ChangeSlide(1)", 5000);
 }
